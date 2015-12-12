6865f8321025d5c8ef976a4e3a5ae16169cef92b
Cojoc Lorena <lorena.cojoc@yahoo.com>
2015-12-12 17:51:47 +0200

    Tema 4

--- proiect/spring-java-app/initial/src/main/java/hello/models/Product.java
+++ proiect/spring-java-app/initial/src/main/java/hello/models/Product.java
@@ -5,11 +5,13 @@ public class Product {
     private  int id;
     private  String name;
     private  double price;
+    private String details;
 
-    public Product(int id, String name,double price) {
+    public Product(int id, String name,double price,String details) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.details = details;
     }
 
     public int getId() {
@@ -26,5 +28,9 @@ public class Product {
 
     public void setPrice(double price){
       this.price = price;
+    }
+
+    public String getDetails() {
+      return details;
     }
 }
